8c028283723521e3db05406df8b8a1d9d46ab091
Pragya Singh <pragyasingh56373@gmail.com>
2026-03-20 21:20:14 +0530

    Add greeting function and Zen of Python import

---
+++ day29_style_.py
@@ -0,0 +1,16 @@
+ """
+Day 29 - Docstrings, PEP 8, and Zen of Python
+"""
+
+# Zen of Python
+import this
+
+
+def greet(name):
+    """This function greets the user."""
+    print(f"Hello, {name}!")
+
+
+# Main program
+user_name = input("Enter your name: ")
+greet(user_name) 
